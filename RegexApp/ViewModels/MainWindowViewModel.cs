578285d5f8899a7390edb051f55f66ca6de80103
Kochtyan <messka.sanfirov@inbox.ru>
2022-03-17 00:18:50 +0700

    trash

---
+++ RegexApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using System.Text.RegularExpressions;
+
+namespace RegexApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        string regex = "";
+        string input = "";
+        string output = "";
+        public string MyRegex
+        {
+            get => regex;
+            set
+            {
+                if (value != null)
+                {
+                    regex = value;
+                }
+            }
+        }
+
+        public string Output
+        {
+            get => output;
+            set
+            {
+                if (MyRegex != "")
+                {
+                    this.RaiseAndSetIfChanged(ref output, value);
+                }
+                else
+                {
+                    this.RaiseAndSetIfChanged(ref output, "Ðåãóëÿðíîå âûðàæåíèå íå ââåäåíî!");
+                }
+            }
+        }
+
+        public string Input
+        {
+            get => input;
+            set
+            {
+                Output = "";
+                if (MyRegex != "")
+                {
+                    Regex rgx = new Regex(MyRegex);
+                    foreach (Match match in rgx.Matches(value))
+                    {
+                        Output += match.Value + "\n";
+                    }
+                    if (Output == "")
+                    {
+                        Output = "Íè÷åãî íå íàéäåíî!";
+                    }
+                }
+                this.RaiseAndSetIfChanged(ref input, value);
+            }
+        }
+    }
+}
